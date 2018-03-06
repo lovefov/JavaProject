cc06c5c9559b504e069227d96f926022f5b60049
jiangchao <654747873@163.com>
2018-03-06 18:30:06 +0800

    2018.3.6

--- src/fanxing/Main.java
+++ src/fanxing/Main.java
@@ -1,5 +1,7 @@
 package fanxing;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +11,7 @@ public class Main {
      * @param args 呵呵呵呵
      */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.println("Hello World!");
         List<String> list= new ArrayList<>();
         list.add("lalala");
@@ -34,7 +36,14 @@ public class Main {
         }
         /*
           foreach 你给迭代器一个中元素起个通用的名字然后对他做相应的操作  map比这个强一百倍..
-         */
+                */
+        String path="./123.txt";
+        File src = new File(path);
+        boolean flag= src.canRead();
+        System.out.println(flag?"可读":"不可读");
+        boolean flag1 = src.canWrite();
+        System.out.println(flag1?"可写":"不可写");
+        boolean flag3 = src.    createNewFile();
 
     }
 }
